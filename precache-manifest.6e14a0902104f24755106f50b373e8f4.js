819be2b3825e621b9aef6f35ac803892d3dc1948
faouziedrissi <faouzi.edrissi@gmail.com>
2020-01-20 00:18:22 +0100

    Updates

---
+++ precache-manifest.6e14a0902104f24755106f50b373e8f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4550dad71ab78c2a1c6990236f67dbc6",
+    "url": "/TODOList/index.html"
+  },
+  {
+    "revision": "7b0504ead983b82ac73e",
+    "url": "/TODOList/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c9d55945f5f25aa8e1cb",
+    "url": "/TODOList/static/css/main.eff1214c.chunk.css"
+  },
+  {
+    "revision": "7b0504ead983b82ac73e",
+    "url": "/TODOList/static/js/2.c9bd8128.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/TODOList/static/js/2.c9bd8128.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9d55945f5f25aa8e1cb",
+    "url": "/TODOList/static/js/main.ba4cc378.chunk.js"
+  },
+  {
+    "revision": "6ba404046ca5ae9272d9",
+    "url": "/TODOList/static/js/runtime-main.9929f03c.js"
+  }
+]);
